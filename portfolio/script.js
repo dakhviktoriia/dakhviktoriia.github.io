b59944e347dff8caef50e6e14b4ac3ef026bf6c9
Viktoriia Dakh <dakhviktoriia@users.noreply.github.com>
2020-01-21 21:13:06 +0200

    commit cvv

---
+++ portfolio/script.js
@@ -0,0 +1,27 @@
+
+
+document.querySelector('.menuB').onclick = function(){
+    console.log('la la laa laa');
+    document.querySelector('.burger-menu-menu').style.display = 'block';
+    document.querySelector('.closeB').style.display = 'block';
+    document.querySelector('.menuB').style.display = 'none';
+
+    
+}
+
+document.querySelectorAll('.ul-burger li').forEach(function(el){
+    el.onclick = function(){
+        document.querySelector('.burger-menu-menu').style.display = 'none';
+        document.querySelector('.closeB').style.display = 'none';
+        document.querySelector('.menuB').style.display = 'block';
+    }
+    
+})
+
+
+document.querySelector('.closeB').onclick = function(){
+    document.querySelector('.burger-menu-menu').style.display = 'none';
+    document.querySelector('.closeB').style.display = 'none';
+    document.querySelector('.menuB').style.display = 'block';
+}
+
